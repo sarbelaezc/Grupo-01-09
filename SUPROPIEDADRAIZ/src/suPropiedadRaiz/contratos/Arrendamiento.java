a8da7760d41b28036b4789aab7483cbcb7e83ffb
JohnJAlpala <jjalpalac@unal.edu.co>
2016-09-18 17:54:34 -0500

    Update Arrendamiento.java

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/contratos/Arrendamiento.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/contratos/Arrendamiento.java
@@ -1,5 +1,14 @@
 package suPropiedadRaiz.contratos;
 
 public class Arrendamiento {
+	int ID;
+	boolean casa, apartamento;
+	String Cliente_Oferente;
+	String Cliente_Arrendatario;
+	String Fecha_de_Inicio;
+	int Valor_Mensual;
+	
+
+
 
 }
