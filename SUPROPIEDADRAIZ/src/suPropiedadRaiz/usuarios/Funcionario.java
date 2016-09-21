559340e2b0a3fe5810fd85d7bfb0ff43b3157d5e
samontoyapa <samontoyapa@unal.edu.co>
2016-09-21 17:32:37 -0500

    Update Funcionario.java

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Funcionario.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Funcionario.java
@@ -1,6 +1,9 @@
 package suPropiedadRaiz.usuarios;
 
+import java.util.ArrayList;
+
 public class Funcionario {
 	long cedula, salario;
 	String nombre;
+	ArrayList <Funcionario> Funcionarios = new ArrayList<Funcionario>();
 }
