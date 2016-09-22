853d31a6b9bdfb53b0508a79ff3ba784c93b93e3
samontoyapa <samontoyapa@unal.edu.co>
2016-09-22 01:36:49 -0500

    Update Funcionario.java

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Funcionario.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Funcionario.java
@@ -4,4 +4,21 @@ public class Funcionario {
 	long cedula, salario, clientes;
 	String nombre;
 
+	public Funcionario(String nombre, long cedula, long salario) {
+		
+	}
+	public long ingresarCedula(){
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ingrese su número de cedula: ");
+		cedula = entrada.nextLong();
+		return cedula;
+	}
+	
+	public String ingresarNombre(){
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ingrese su nombre: ");
+		nombre = entrada.nextLine();
+		return nombre;
+	}
+
 }
