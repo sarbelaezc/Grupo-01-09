ccd773fcc1c27e41c26d51f2af777c3d776535c9
SchavarriaG <schavarriag@unal.edu.co>
2016-09-28 16:17:56 -0500

    Agregar cosas
    Método ingresar salario, constructor por defecto y las asignaciones del constructor public Funcionario(String nombre, long cedula, long salario).

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Funcionario.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Funcionario.java
@@ -8,9 +8,18 @@ public class Funcionario {
 	long cedula, salario, clientes;
 	String nombre;
 	ArrayList<Cliente> clientesList = new ArrayList<Cliente>();
-
+	ArrayList <ClienteRegistrado> ClienteList = new ArrayList<ClienteRegistrado>();
+	
 	public Funcionario(String nombre, long cedula, long salario) {
-		
+		this.nombre = nombre; 
+		this.cedula = cedula; 
+		this.salario = salario;
+	}
+	
+	public Funcionario() {
+		nombre = ingresarNombre();
+		cedula = ingresarCedula();
+		salario = ingresarSalario();
 	}
 	
 	public long ingresarCedula(){
@@ -25,6 +34,13 @@ public class Funcionario {
 		System.out.print("Ingrese su nombre: ");
 		nombre = entrada.nextLine();
 		return nombre;
+	}
+	
+	public long ingresarSalario(){
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ingrese su salario: ");
+		salario = entrada.nextLong();
+		return salario;
 	}
 	
 	public void Arrendar(){
