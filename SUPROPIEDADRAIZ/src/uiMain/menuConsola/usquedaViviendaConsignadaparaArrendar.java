104c04bcf9cd546c8cb399d35498ae9db3bcd79f
luisguillermo786 <lgazap@unal.edu.co>
2016-10-06 17:57:06 -0500

    Create usquedaViviendaConsignadaparaArrendar.java
    Función que falta implementar para la búsqueda del usuario de viviendas para arrendar

---
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/usquedaViviendaConsignadaparaArrendar.java
@@ -0,0 +1,11 @@
+package uiMain.menuConsola;
+
+public class BusquedaViviendaConsignadaparaArrendar extends OpcionDeMenu {
+
+	public void ejecutar(){
+		
+	}
+	public String toString(){
+		return "Búsqueda de viviendas para Arrendar";
+	}
+}
