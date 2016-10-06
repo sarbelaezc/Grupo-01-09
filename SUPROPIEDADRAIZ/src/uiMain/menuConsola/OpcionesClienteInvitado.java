a3891abe622b14b9a69941595c05194cc697aa87
luisguillermo786 <lgazap@unal.edu.co>
2016-10-06 17:42:09 -0500

    Update OpcionesClienteInvitado.java

--- SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionesClienteInvitado.java
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionesClienteInvitado.java
@@ -1,12 +1,25 @@
-package uiMain.menuConsola;
+import java.util.Scanner;
+
+import suPropiedadRaiz.usuarios.ClienteAnonimo;
 
 public class OpcionesClienteInvitado extends OpcionDeMenu {
+			
+ ClienteAnonimo anonimo;
 	
-	@Override
-	public void ejecutar() {
-		
+	public OpcionesClienteInvitado() {
+		anonimo = new ClienteAnonimo();
 	}
 
+	public void ejecutar() {
+		
+		MenuDeConsola.limpiarLista();
+		
+		MenuDeConsola.anadirOpcion(new SolicitarCita());
+		
+		MenuDeConsola.lanzarMenu();
+	}
+	
+		
 	@Override
 	public String toString() {
 		return "Cliente Invitado";
