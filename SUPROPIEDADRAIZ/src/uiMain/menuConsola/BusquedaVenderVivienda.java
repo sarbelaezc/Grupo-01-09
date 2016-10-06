5db89cfd9d8d24a83bda9f4b136e58c6cb92e8a2
luisguillermo786 <lgazap@unal.edu.co>
2016-10-06 17:54:35 -0500

    Create BusquedaVenderVivienda.java
    Función que falta implementar para la búsqueda de viviendas consignadas por el usuario para la venta

---
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/BusquedaVenderVivienda.java
@@ -0,0 +1,9 @@
+public class BusquedaVenderVivienda extends OpcionDeMenu {
+    public void ejecutar(){
+    	
+    }
+	
+	public String toString(){
+		return "Búsqueda de Viviendas consignadas para la venta";
+	}
+}
