78256a6b1b59babd71d7f51a9e390f5a1f54dfba
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-09 22:45:21 -0500

    Gestor de archivos inicial

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/gestorBD/GestorArchivos.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/gestorBD/GestorArchivos.java
@@ -2,6 +2,7 @@ package suPropiedadRaiz.gestorBD;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,16 +12,42 @@ import suPropiedadRaiz.usuarios.Cliente;
 public class GestorArchivos {
 
 	 public static void crear(){
-		 Cliente cliente = new Cliente();
-	     ObjectOutputStream salida;
+		 Cliente cliente=new Cliente(1037650445, "Santiago", "Prueba");
+		 ObjectOutputStream salida;
 		try {
-			salida = new ObjectOutputStream (new FileOutputStream("Cliente.txt"));
+			salida = new ObjectOutputStream(new FileOutputStream("Clientes.obj"));
+			salida.writeObject("Datos del cliente\n");
 			salida.writeObject(cliente);
-		    salida.close();
-		} catch (FileNotFoundException e) {
+			salida.close();
+			
+			System.out.println("Se ha creado Clientes.obj");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		 
+	 }
+	 
+	 public static void leer() throws ClassNotFoundException{
+		ObjectInputStream entrada;
+		
+		try {
+			entrada = new ObjectInputStream(new FileInputStream("Clientes.obj"));
+			String str=(String)entrada.readObject();
+			Cliente obj1=(Cliente)entrada.readObject();
+			System.out.println("------------------------------");
+			System.out.println(str+obj1.getNombre());
+			System.out.println("------------------------------");
+			entrada.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("El archivo no existe");
+			crear();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	 }
 }
