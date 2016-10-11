0d34c75a72acacd04b7466e3da983388e198aba7
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-10 22:03:39 -0500

    caso de prueba

--- SUPROPIEDADRAIZ/src/uiMain/SuPropiedadRaiz.java
+++ SUPROPIEDADRAIZ/src/uiMain/SuPropiedadRaiz.java
@@ -1,12 +1,15 @@
 package uiMain;
 
+import java.io.IOException;
+
 import suPropiedadRaiz.gestorBD.GestorArchivos;
 
 import uiMain.menuConsola.MenuDeConsola;
 
 public class SuPropiedadRaiz {
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		GestorArchivos.crearListaPersonas();
 		GestorArchivos.leer();
 		System.out.println("/-/-/-/-/-/-/-/-/-/-/Su Propiedad Raiz/-/-/-/-/-/-/-/-/-/-/");
 		@SuppressWarnings("unused")
