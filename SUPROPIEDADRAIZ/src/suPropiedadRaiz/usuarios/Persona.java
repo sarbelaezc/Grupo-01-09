84326f251953514d45b0aefd2cf072a92b4613f6
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-10 22:03:19 -0500

    Serializacion de persona

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Persona.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Persona.java
@@ -1,10 +1,10 @@
 package suPropiedadRaiz.usuarios;
 
-public class Persona {
+import java.io.Serializable;
+
+public abstract class Persona implements Serializable {
+
+	private static final long serialVersionUID = 7543457343574046297L;
 	long cedula;
 	String nombre;
-	
-	public Persona(){
-		
-	}
 }
