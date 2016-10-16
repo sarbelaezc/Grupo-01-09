6ee93cebcd51905e0f12e000af4e45fc58104e1d
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-16 14:04:25 -0500

    Añadidas opciones necesarias para lalectura de datos

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Persona.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Persona.java
@@ -1,10 +1,16 @@
 package suPropiedadRaiz.usuarios;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+
+import uiMain.menuConsola.OpcionDeMenu;
 
 public abstract class Persona implements Serializable {
 
 	private static final long serialVersionUID = 7543457343574046297L;
 	long cedula;
 	String nombre;
+	public ArrayList<OpcionDeMenu> opciones;
+	
+	public abstract long getCedula();
 }
