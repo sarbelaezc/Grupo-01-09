b40538e92825a38c024cdb3cf163545b9e3f47bf
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-16 14:03:08 -0500

    Constructor de administrador y get y set para sus atributos

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Administrador.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/usuarios/Administrador.java
@@ -5,10 +5,30 @@ import uiMain.menuConsola.*;
 
 public class Administrador extends Persona {
 	
+	private static final long serialVersionUID = 6885455392745744589L;
 	String nombre;
+	long cedula;
 	ArrayList<OpcionDeMenu> opciones = new ArrayList<OpcionDeMenu>();
 	
-	public Administrador(){
-		this.nombre = "Administrador";
+	public Administrador(String nombre, long cedula){
+		this.nombre = nombre;
+		this.cedula = cedula;
+	}
+
+	@Override
+	public long getCedula() {
+		return this.cedula;
+	}
+	
+	public void setCedula(long cedula){
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre = nombre;
 	}
 }
