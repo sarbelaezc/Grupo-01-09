afabdbd492d3f6617073dd9594fed156032befb7
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-18 19:57:05 -0500

    Menu inicial

---
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionUsuarioInvitado.java
@@ -0,0 +1,41 @@
+package uiMain.menuConsola;
+
+import java.util.Scanner;
+
+import suPropiedadRaiz.gestorBD.GestorArchivos;
+import suPropiedadRaiz.usuarios.Cliente;
+
+/**
+ * Esta clase representa al usuario anonimo o invitado, pregunta lso datos, lo crea y lo almacena para el gestor de archivos
+ * @author Santiago Arbelaez Cataño
+ * @author Sebastian Chavarría Gómez
+ * @author Santiago Montoya Palacio
+ */
+public class OpcionUsuarioInvitado extends OpcionDeMenu {
+
+	private static final long serialVersionUID = -176536618825282299L;
+
+	@Override
+	public void ejecutar() {
+		
+		@SuppressWarnings("resource")
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Ingrese su nombre: ");
+		String nombre = entrada.nextLine();
+		
+		System.out.print("Ingrese su cedula: ");
+		long cedula = entrada.nextLong();
+		
+		System.out.print("Ingrese su motivo: ");
+		String motivo = entrada.next();
+		
+		GestorArchivos.personas.add(new Cliente(cedula, nombre, motivo));
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario Invitado";
+	}
+
+}
