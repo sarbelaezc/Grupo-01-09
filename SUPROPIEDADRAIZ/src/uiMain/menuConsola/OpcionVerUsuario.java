e16cf132c13f28606767e6b913ebda8bfa599829
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-18 19:59:42 -0500

    Comentario

--- SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionVerUsuario.java
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionVerUsuario.java
@@ -6,14 +6,21 @@ import java.util.Scanner;
 import suPropiedadRaiz.gestorBD.GestorArchivos;
 import suPropiedadRaiz.usuarios.Persona;
 
+/**
+ * Esta clase representa la opcion del administrador de ver las opciones como si fuera un usuario dado
+ * @author Santiago Arbeláez Cataño
+ * @author Sebastian Chavarría Gómez
+ * @author Santiago Montoya Palacio
+ */
 public class OpcionVerUsuario extends OpcionDeMenu {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 2075469877557187418L;
 	Persona persona;
 	
+	/**
+	 * Este metodo ejecuta el menu de un usuario dado
+	 * @see uiMain.menuConsola.OpcionDeMenu#ejecutar()
+	 */
 	@Override
 	public void ejecutar() {
 		
