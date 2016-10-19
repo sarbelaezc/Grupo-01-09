cbf3900d1af44a4a4494d18891fb411071e030b7
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-18 22:24:40 -0500

    COmentarios y opciones implementadas hasta el momento

--- SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionesExistentes.java
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionesExistentes.java
@@ -1,17 +1,43 @@
 package uiMain.menuConsola;
 
+/**
+ * Esta clase es la encargada de mostrar y alojar las opciones existentes en el sistema sin importar el usuario
+ * @author Santiago Arbeláez Cataño
+ * @author Sebastian Chavarría Gómez
+ * @author Santiago Montoya Palacio
+ */
 public class OpcionesExistentes extends OpcionDeMenu {
 
 	private static final long serialVersionUID = 957442375590288316L;
-
+	MenuDeConsola menu = new MenuDeConsola();
+	
+	public OpcionesExistentes(){
+		
+		menu.anadirOpcion(new OpcionEliminarDeUsuario());
+		menu.anadirOpcion(new OpcionesExistentes());
+		menu.anadirOpcion(new OpcionNuevaUsuario());
+		menu.anadirOpcion(new OpcionSalir());
+		menu.anadirOpcion(new OpcionUsuarioInvitado());
+		menu.anadirOpcion(new OpcionUsuarioRegistrado());
+		menu.anadirOpcion(new OpcionVerUsuario());	
+	}
+	
 	@Override
 	public void ejecutar() {
 		
+		menu.anadirOpcion(new OpcionEliminarDeUsuario());
+		menu.anadirOpcion(new OpcionesExistentes());
+		menu.anadirOpcion(new OpcionNuevaUsuario());
+		menu.anadirOpcion(new OpcionSalir());
+		menu.anadirOpcion(new OpcionUsuarioInvitado());
+		menu.anadirOpcion(new OpcionUsuarioRegistrado());
+		menu.anadirOpcion(new OpcionVerUsuario());	
+		
+		menu.lanzarMenu();
 	}
 
 	@Override
 	public String toString() {
-		return "Opciones Existentes";
+		return "Opciones Existentes <<<<<<<<<<<<<<<<<< NOT YET >>>>>>>>>>>>>>>>>>";
 	}
-
 }
