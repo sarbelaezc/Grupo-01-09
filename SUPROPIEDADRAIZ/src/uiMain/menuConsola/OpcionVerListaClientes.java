4b1756d330346ebb1c4e308504f5dcb4757d754e
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-20 17:01:23 -0500

    Opcion para ver la lsita de clientes del funcionario, comentada

--- SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionVerListaClientes.java
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionVerListaClientes.java
@@ -1,11 +1,40 @@
 package uiMain.menuConsola;
 
+import java.util.ArrayList;
+
+import suPropiedadRaiz.usuarios.Cliente;
+import suPropiedadRaiz.usuarios.Funcionario;
+
+/**
+ * Esta clase representa la opcion del funcionario de ver todos los clientes que tiene a cargo
+ * @author Santiago Arbeláez Cataño
+ * @author Sebastian Chavarría Gómez
+ * @author Santiago Montoya Palacio
+ */
 @SuppressWarnings("serial")
 public class OpcionVerListaClientes extends OpcionDeMenu {
+
+	Funcionario f;
+	ArrayList<Cliente> listaClientes;
+	
+	public OpcionVerListaClientes(Funcionario funcionario) {
+		// TODO Auto-generated constructor stub
+		f = funcionario;
+	}
+
+	public OpcionVerListaClientes() {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void ejecutar() {
 		// TODO Auto-generated method stub
+		listaClientes = f.getClientesList();
+		System.out.println("----------Clientes----------");
+		for(int i=0; i<listaClientes.size(); i++){
+			Cliente c = listaClientes.get(i);
+			System.out.println("C.C. " + c.getCedula() + " Nombre: " + c.getNombre());
+		}
 
 	}
 
