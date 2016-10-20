65e19960acfe06762a7fab2db97ea7e68cd84d5b
sarbelaezc <sarbelaezc@unal.edu.co>
2016-10-19 19:35:39 -0500

    Constructor de opciones existentes corregido

--- SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionesExistentes.java
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionesExistentes.java
@@ -1,7 +1,5 @@
 package uiMain.menuConsola;
 
-import suPropiedadRaiz.usuarios.OpcionNuevoFuncionarioJ;
-
 /**
  * Esta clase es la encargada de mostrar y alojar las opciones existentes en el sistema sin importar el usuario
  * @author Santiago Arbeláez Cataño
@@ -15,18 +13,19 @@ public class OpcionesExistentes extends OpcionDeMenu {
 	
 	public OpcionesExistentes(){
 		
-		menu.anadirOpcion(new OpcionEliminarDeUsuario());
-		menu.anadirOpcion(new OpcionesExistentes());
-		menu.anadirOpcion(new OpcionNuevaUsuario());
-		menu.anadirOpcion(new OpcionSalir());
-		menu.anadirOpcion(new OpcionUsuarioInvitado());
-		menu.anadirOpcion(new OpcionUsuarioRegistrado());
-		menu.anadirOpcion(new OpcionVerUsuario());	
-		menu.anadirOpcion(new OpcionNuevoFuncionarioJ());
+		this.menu.anadirOpcion(new OpcionEliminarDeUsuario());
+		this.menu.anadirOpcion(new OpcionNuevaUsuario());
+		this.menu.anadirOpcion(new OpcionSalir());
+		this.menu.anadirOpcion(new OpcionUsuarioInvitado());
+		this.menu.anadirOpcion(new OpcionUsuarioRegistrado());
+		this.menu.anadirOpcion(new OpcionVerUsuario());	
+		this.menu.anadirOpcion(new OpcionNuevoFuncionarioJ());
 	}
 	
 	@Override
 	public void ejecutar() {
+		
+		menu.limpiarLista();
 		
 		menu.anadirOpcion(new OpcionEliminarDeUsuario());
 		menu.anadirOpcion(new OpcionesExistentes());
