e20a8ed6eb3a23baa1d101564149b262ca260695
samontoyapa <samontoyapa@unal.edu.co>
2016-10-20 18:20:52 -0500

    Implementacion metodo ejecutar

--- SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionAsignarRolCliente.java
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionAsignarRolCliente.java
@@ -1,13 +1,34 @@
 package uiMain.menuConsola;
+import java.io.IOException;
+import java.util.Scanner;
+import suPropiedadRaiz.gestorBD.GestorArchivos;
+import suPropiedadRaiz.usuarios.*;
 
 public class OpcionAsignarRolCliente extends OpcionDeMenu {
 
 	private static final long serialVersionUID = -3375878365254753626L;
-
+	Persona persona;
+	Cliente clt;
 	@Override
 	public void ejecutar() {
-		// TODO Auto-generated method stub
-
+		
+		@SuppressWarnings("resource")
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ingrese la ID del cliente");
+		long ID = entrada.nextLong();
+		
+		try{
+			persona = GestorArchivos.buscar(ID);
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		clt= (Cliente)persona;
+		
+		System.out.print("Ingrese el nuevo motivo");
+		String M = entrada.nextLine();
+		clt.setMotivo(M);
 	}
 
 	@Override
