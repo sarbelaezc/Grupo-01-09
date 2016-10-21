c48fba799da3e68767a0500ded48f99c2664836f
samontoyapa <samontoyapa@unal.edu.co>
2016-10-21 04:26:56 -0500

    OpcionesUsuarioInvi

--- SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionUsuarioInvitado.java
+++ SUPROPIEDADRAIZ/src/uiMain/menuConsola/OpcionUsuarioInvitado.java
@@ -1,13 +1,17 @@
 package uiMain.menuConsola;
 
 import java.util.Scanner;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 import suPropiedadRaiz.gestorBD.GestorArchivos;
 import suPropiedadRaiz.usuarios.Cliente;
+import suPropiedadRaiz.usuarios.Funcionario;
+import suPropiedadRaiz.usuarios.FuncionarioJefe;
 import suPropiedadRaiz.usuarios.Persona;
 
 /**
- * Esta clase representa al usuario anonimo o invitado, pregunta lso datos, lo crea y lo almacena para el gestor de archivos
+ * Esta clase representa al usuario anonimo o invitado, pregunta los datos, lo crea y lo almacena para el gestor de archivos
  * @author Santiago Arbelaez Cataño
  * @author Sebastian Chavarría Gómez
  * @author Santiago Montoya Palacio
@@ -15,7 +19,7 @@ import suPropiedadRaiz.usuarios.Persona;
 public class OpcionUsuarioInvitado extends OpcionDeMenu {
 
 	private static final long serialVersionUID = -176536618825282299L;
-
+	String motivo;
 	@Override
 	public void ejecutar() {
 		
@@ -28,12 +32,54 @@ public class OpcionUsuarioInvitado extends OpcionDeMenu {
 		System.out.print("Ingrese su cedula: ");
 		long cedula = entrada.nextLong();
 		
-		System.out.print("Ingrese su motivo: ");
-		String motivo = entrada.next();
+		System.out.print("Ingrese el motivo de la cita");
+		System.out.print("[1] Arrendar");
+		System.out.print("[2] Comprar");
+		System.out.print("[3] Ambas");
+		
+		int MO = entrada.nextInt();
+
+		if(MO == 1){
+			motivo = "Arrendar";
+		}
+		else if (MO==2){
+			motivo= "Comprar";
+		}
+		else if(MO==3){
+			motivo = "Ambas";
+		}
+		else
+		{
+			System.out.print("Opcion Invalida");
+		}
 		
 		Persona p = new Cliente(cedula, nombre, motivo);
-		
-		GestorArchivos.personas.add(p);
+		try {
+			Persona p2 = GestorArchivos.buscar(cedula);
+			if(p2 == null){
+				FuncionarioJefe funJefe;
+				ArrayList<Funcionario> funci = new ArrayList<Funcionario>();
+				GestorArchivos.personas.add(p);
+				for(int i =0; i< GestorArchivos.personas.size(); i++){
+					if(GestorArchivos.personas.get(i) instanceof FuncionarioJefe){
+						funJefe = (FuncionarioJefe)GestorArchivos.personas.get(i);
+						funci = funJefe.ListaFuncionarios();
+					}
+					
+					
+				}
+			}
+			GestorArchivos.personas.add(p);
+				if(.getCitasList().size()<16){
+					.setCita(cedula);	
+				}
+			
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -42,3 +88,4 @@ public class OpcionUsuarioInvitado extends OpcionDeMenu {
 	}
 
 }
+
