7b49047b3dd323cb13251ce2a302143cf0e78805
sarbelaezc <sarbelaezc@unal.edu.co>
2016-11-14 13:53:14 -0500

    Excepcion padre de todas las excepciones que vamos a crear

---
+++ SUPROPIEDADRAIZ/src/excepciones/ExceptionSuPropiedadRaiz.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+@SuppressWarnings("serial")
+public class ExceptionSuPropiedadRaiz extends Exception {
+
+	public ExceptionSuPropiedadRaiz(String string) {
+		super(string);
+	}
+}
