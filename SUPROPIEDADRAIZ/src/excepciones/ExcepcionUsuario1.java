3dc1322b5be3a25b7c0565bc89ae288aad5ac44b
sarbelaezc <sarbelaezc@unal.edu.co>
2016-11-15 23:30:02 -0500

    reempaquetado de clases

--- SUPROPIEDADRAIZ/src/excepciones/ExcepcionUsuario1.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class ExcepcionUsuario1 extends ExcepcionUsuario {
-	
-	public ExcepcionUsuario1(){
-		super("Los campos de nombre de usuario o contraseña \n no han sido diligenciados correctamente.");
-	}
-}
--- SUPROPIEDADRAIZ/src/excepciones/ExceptionSuPropiedadRaiz.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class ExceptionSuPropiedadRaiz extends Exception {
-
-	public ExceptionSuPropiedadRaiz(String string) {
-		super(string);
-	}
-}
