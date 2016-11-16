16bc0698eaf967fdee20a0564a69c2d00e6067ad
sarbelaezc <sarbelaezc@unal.edu.co>
2016-11-15 23:29:10 -0500

    Excepcion de nombre o contraseña invalidos

---
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/excepciones/ExcepcionUsuario2.java
@@ -0,0 +1,10 @@
+package suPropiedadRaiz.excepciones;
+
+@SuppressWarnings("serial")
+public class ExcepcionUsuario2 extends ExcepcionUsuario {
+
+	public ExcepcionUsuario2() {
+		super("Nombre de usuario o contraseña no validos");
+	}
+
+}
