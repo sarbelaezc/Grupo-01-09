b6783dc802ee0cbaf6a89745f2bc37822e4e774d
sarbelaezc <sarbelaezc@unal.edu.co>
2016-11-20 17:14:52 -0500

    Añadido atributo de imagen para inmuebles

--- SUPROPIEDADRAIZ/src/suPropiedadRaiz/inmuebles/Inmuebles.java
+++ SUPROPIEDADRAIZ/src/suPropiedadRaiz/inmuebles/Inmuebles.java
@@ -1,19 +1,32 @@
 package suPropiedadRaiz.inmuebles;
 
+import java.io.Serializable;
+
+import javax.swing.ImageIcon;
+
 /**
  * Clase que permite agrupar apartamento y casa bajo el mismo tipo de objeto
  * @author Santiago Arbeláez Cataño
  * @author Sebastian Chavarría Gómez
  * @author Santiago Montoya Palacio
  */
-public abstract class Inmuebles {
-
-public abstract void setenVenta(boolean b);
-
-public abstract void setArrendar(boolean b);
-
-public abstract int getCodigo();	
-
+@SuppressWarnings("serial")
+public abstract class Inmuebles implements Serializable{
 	
+	ImageIcon imagen;
+
+	public abstract void setenVenta(boolean b);
+
+	public abstract void setArrendar(boolean b);
+
+	public abstract int getCodigo();
+
+	public ImageIcon getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(ImageIcon imagen) {
+		this.imagen = imagen;
+	}	
 	
 }
