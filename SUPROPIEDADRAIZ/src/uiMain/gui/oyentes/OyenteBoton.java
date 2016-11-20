ff25946b7acb585d089060d46c08cd43cd362dc7
sarbelaezc <sarbelaezc@unal.edu.co>
2016-11-20 17:17:26 -0500

    Comentarios necesarios para documentacion

--- SUPROPIEDADRAIZ/src/uiMain/gui/oyentes/OyenteBoton.java
+++ SUPROPIEDADRAIZ/src/uiMain/gui/oyentes/OyenteBoton.java
@@ -4,8 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -19,20 +17,42 @@ import suPropiedadRaiz.usuarios.Administrador;
 import suPropiedadRaiz.usuarios.Cliente;
 import suPropiedadRaiz.usuarios.Persona;
 import uiMain.gui.GUIAdministrador;
+import uiMain.gui.GUIAnonimo;
 import uiMain.gui.GUIUsuario;
 
+/**
+ * Esta clase representa todos los oyentes de los botones del programa
+ * @author Santiago Arbeláez Cataño
+ * @author Sebastian Chavarría Gómez
+ * @author Santiago Montoya Palacio
+ */
 public class OyenteBoton implements ActionListener {
 	
+	GUIAnonimo V1;
 	JButton B1;
 	JLabel L1, L2, L3;
 	JTextField T1, T2;
 	int count = 1;
 	
+	/**
+	 * Constructor default
+	 */
 	public OyenteBoton() {
 
 	}
-
-	public OyenteBoton(JButton B, JLabel L1, JLabel L2, JLabel L3, JTextField T1, JTextField T2) {
+	
+	/**
+	 * Constructor que permite cambiar interactuar con los parametros que se le estan pasando
+	 * @param B
+	 * @param L1
+	 * @param L2
+	 * @param L3
+	 * @param T1
+	 * @param T2
+	 * @param V1
+	 */
+	public OyenteBoton(JButton B, JLabel L1, JLabel L2, JLabel L3, JTextField T1, JTextField T2, GUIAnonimo V1) {
+		this.V1 = V1;
 		this.B1 = B;
 		this.L1 = L1;
 		this.L2 = L2;
@@ -41,6 +61,11 @@ public class OyenteBoton implements ActionListener {
 		this.T2 = T2;
 	}
 	
+	/* 
+	 * Metodo por medio del cual se ejecutan los clics a los botones
+	 * (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String actionCommand = e.getActionCommand();
@@ -48,15 +73,22 @@ public class OyenteBoton implements ActionListener {
 		if(e.getSource() instanceof JButton){
 			
 			if("Salir".equals(actionCommand)){
+				
 				int resp = JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea salir?", "¡Saliendo!", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
 				
-				if(resp == 0){
+				if(resp == 0){	
+					try {
+						GestorArchivos.crearListaPersonas();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
 					System.exit(0);
+					
 				}else if(resp == 1){
 					
 				}
 			}else if("Ingresar".equals(actionCommand)){
-				B1.setText("<html><p align='center'>Complete informacion y de nuevo clic</p></html>");
+				B1.setText("<html><p align='center'>Complete la informacion y click otra vez</p></html>");
 				B1.setActionCommand("Continuar");
 				
 				L1.setVisible(true);
@@ -74,28 +106,30 @@ public class OyenteBoton implements ActionListener {
 					try {
 						throw new ExcepcionUsuario1();
 					} catch (ExcepcionUsuario1 e1) {
-						// TODO Auto-generated catch block
 						JOptionPane.showConfirmDialog(null, e1.getMessage(), "¡ERROR!", JOptionPane.PLAIN_MESSAGE, JOptionPane.ERROR_MESSAGE);
 					}
-				}else{
+				}else{										
+					try {
 						long user = Long.parseLong(usuario);
-						
-						try {
-							Persona p = GestorArchivos.buscar(user);
+							
+						Persona p = GestorArchivos.buscar(user);
 
-							if(p == null){
-								throw new ExcepcionUsuario2();
-							}else if(p instanceof Administrador){
-								new GUIAdministrador();
-								
-							}else if(p instanceof Cliente){
-								new GUIUsuario();
-								
-							}
-						} catch (ClassNotFoundException | IOException | ExcepcionUsuario2 e1) {
-							JOptionPane.showConfirmDialog(null, e1.getMessage(), "¡ERROR!", JOptionPane.PLAIN_MESSAGE, JOptionPane.ERROR_MESSAGE);
+						if(p == null){
+							throw new ExcepcionUsuario2();
+						}else if(p instanceof Administrador){
+							new GUIAdministrador(p);
+							V1.setVisible(false);
+							
+						}else if(p instanceof Cliente){
+							new GUIUsuario();
+							V1.setVisible(false);
+							
 						}
-						
+					} catch (ClassNotFoundException | IOException | ExcepcionUsuario2 e1) {
+						JOptionPane.showConfirmDialog(null, e1.getMessage(), "¡ERROR!", JOptionPane.PLAIN_MESSAGE, JOptionPane.ERROR_MESSAGE);
+					}catch (NumberFormatException e1){
+						JOptionPane.showConfirmDialog(null, "El nombre de usuario debe ser su cedula", "¡ERROR!", JOptionPane.PLAIN_MESSAGE, JOptionPane.ERROR_MESSAGE);
+					}
 				}
 			}else if("Inmuebles".equals(actionCommand)){
 				ArrayList<Persona> personasRegistradas;
