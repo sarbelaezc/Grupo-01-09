2c49b160eba853f5b91ca2f25e2e92bbaae06bc0
sarbelaezc <sarbelaezc@unal.edu.co>
2016-11-23 18:32:03 -0500

    Opcion que no estaba en la guia

--- SUPROPIEDADRAIZ/src/uiMain/gui/GUIAdministrador.java
+++ SUPROPIEDADRAIZ/src/uiMain/gui/GUIAdministrador.java
@@ -30,17 +30,20 @@ public class GUIAdministrador extends JFrame {
 	JPanel P3 = new JPanel();
 	JPanel P4 = new JPanel();
 	JPanel P5 = new JPanel();
+	JPanel P6 = new JPanel();
 	
 	JButton B1 = new JButton("Consulta 1");
 	JButton B2 = new JButton("Consulta 2");
 	JButton B3 = new JButton("Consulta 3");
 	JButton B4 = new JButton("Consulta 4");
+	JButton B5 = new JButton("Consulta 5");
 	
 	JLabel L1 = new JLabel("<HTML><FONT SIZE=15><p align='center'>Bienvenido Administrador</p></FONT></HTML>");
 	JLabel L2 = new JLabel("Listar opciones de menu de un usuario");
 	JLabel L3 = new JLabel("Listar opciones de menu existentes en el sistema");
 	JLabel L4 = new JLabel("Adicionar una nueva opcion de menu");
 	JLabel L5 = new JLabel("Eliminar una opcion de menu");
+	JLabel L6 = new JLabel("Registrar nuevo funcionario jefe");
 	
 	/**
 	 * Constructor que pasa como parametro una persona administrador
@@ -79,7 +82,7 @@ public class GUIAdministrador extends JFrame {
 		salir.addActionListener(new OyenteMenu(this));
 		
 		opciones.setActionCommand("Opciones");
-		opciones.addActionListener(new OyenteMenu(P0,P1,P2,P3,P4,P5,L1,L2,L3,L4,L5,B1,B2,B3,B4));
+		opciones.addActionListener(new OyenteMenu(P0,P1,P2,P3,P4,P5,P6,L1,L2,L3,L4,L5,L6,B1,B2,B3,B4,B5));
 		
 		info_empresa.setActionCommand("Acerca");
 		info_empresa.addActionListener(new OyenteMenu());
@@ -107,13 +110,14 @@ public class GUIAdministrador extends JFrame {
 		/*
 		 * Aca comienza la implementacion de la ventana de adminsitrador
 		 */
-		P0.setLayout(new GridLayout(5,1));
+		P0.setLayout(new GridLayout(6,1));
 		
 		P0.add(P1);
 		P0.add(P2);
 		P0.add(P3);
 		P0.add(P4);
 		P0.add(P5);
+		P0.add(P6);
 		
 		P3.add(L1);
 		
